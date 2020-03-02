81393b8cb539ee737da52e0fa848ca362a02fc91
benoit pepin <pepinbenoit@hotmail.fr>
2020-03-02 09:23:43 +0100

    un reptit push vu que nathan troll

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+import logging
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -7,10 +11,47 @@ sio = SocketIO(app)
 
 sio.run(app)
 
+class Room:
+    def __init__(self):
+        self.players = []
+    
+    def GetPlayerById(self, id):
+        for player in self.players:
+            if(player.sid == id):
+                return player
+        return null
+
+class Player:
+    def __init__(self, sid):
+        self.sid = sid
+        self.pseudo = ''
+        self.ready = 0
+
+
+room = Room()
+
 @app.route('/')
 def hello_world():
     return 'Hello, Worlddd!'
 
-@sio.on("messageSend")
-def messageSend(data):
-    print(data)
+@sio.on("chooseName")
+def chooseName(data):
+    player = room.GetPlayerById(request.sid)
+    print(player.pseudo)
+    player.pseudo = data['pseudo']
+    print(player.pseudo)
+
+@sio.on('connect')
+def connect():
+    room.players.append(Player(request.sid))
+    listePseudo = []
+    for player in room.players:
+        listePseudo.append(player.pseudo)
+    print(listePseudo)
+    print(room.players)
+    emit('listeJoueurs',listePseudo)
+
+@sio.on('disconnect')
+def disconnect():
+    player = room.GetPlayerById(request.sid)
+    room.players.remove(player)
