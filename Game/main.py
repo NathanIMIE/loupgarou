a474b8e560a6b11213259614da7592a1f5b26691
NathanIMIE <nathan.yvon@imie.fr>
2020-03-02 09:18:04 +0100

    Push des familles

--- Game/main.py
+++ Game/main.py
@@ -3,7 +3,7 @@ import socketio
 
 pg.init()
 sio = socketio.Client()
-screen = pg.display.set_mode((400, 400))
+screen = pg.display.set_mode((800, 400))
 COLOR_INACTIVE = (0,0,0)
 COLOR_ACTIVE = (0,0,0)
 FONT = pg.font.Font(None, 32)
@@ -13,7 +13,7 @@ black = (0,0,0)
 green = (0,255,0)
 red = (255,0,0)
 blue = (0,0,128)
-text = FONT.render('Nombre de joueurs', True, black) 
+text = FONT.render('Nom du joueur', True, black) 
 textRect = text.get_rect()
 textRect.center = (200, 25)
 text3 = FONT.render('Nom de la room', True, black) 
@@ -22,6 +22,32 @@ textRect3.center = (200, 125)
 text4 = FONT.render('Mot de passe', True, black) 
 textRect4 = text4.get_rect()
 textRect4.center = (200, 225)
+text5 = FONT.render('Pseudo des joueurs présents', True, black) 
+textRect5 = text5.get_rect()
+textRect5 = (400, 12)
+
+class Listing:
+    def __init__(self, x, y, w, h, listing=[]):
+        self.rect = pg.Rect(x, y, w, h)
+        self.bgColor = black
+        self.listing = listing
+        self.txt_surfaces = []
+        for pseudo in self.listing:
+            print(pseudo)
+            self.txt_surfaces.append(FONT.render(pseudo, True, green))
+
+    def draw(self, screen):
+        y = self.rect.y + 5
+        for txt_surface in self.txt_surfaces:
+            screen.blit(txt_surface, (self.rect.x, y))
+            y += 50
+        pg.draw.rect(screen, self.bgColor, self.rect, 2)
+
+@sio.on('listeJoueurs')
+def listeJoueurs(data):
+    listing = data
+    test = Listing(400,50,100,50,listing)
+    test.draw(screen)
 
 class SubmitButton:
     def __init__(self, x, y, w, h,state, text=''):
@@ -34,10 +60,8 @@ class SubmitButton:
     def handle_event(self, event):
         if event.type == pg.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(event.pos):
-                print(self.state.joueurs)
-                print(self.state.room)
-                print(self.state.mdp)
-                sio.emit("info",{"joueurs":self.state.joueurs,"room":self.state.room,"mdp":self.state.mdp})
+                print(self.state.pseudo)
+                sio.emit("chooseName",{"pseudo":self.state.pseudo})
 
     def update(self):
         pass
@@ -72,20 +96,14 @@ class InputBox:
             if self.active:
                 if event.key == pg.K_RETURN:
                     print(self.text)
-                    print(self.state.joueurs)
-                    print(self.state.room)
-                    print(self.state.mdp)
-                    sio.emit("info",{"joueurs":self.state.joueurs,"room":self.state.room,"mdp":self.state.mdp})
+                    print(self.state.pseudo)
+                    sio.emit("chooseName",{"pseudo":self.state.pseudo})
                 elif event.key == pg.K_BACKSPACE:
                     self.text = self.text[:-1]
                 else:
                     self.text += event.unicode
-                    if self.id == 'joueurs':
-                        self.state.joueurs = self.text
-                    if self.id == 'room':
-                        self.state.room = self.text                        
-                    if self.id == 'mdp':
-                        self.state.mdp = self.text
+                    if self.id == 'pseudo':
+                        self.state.pseudo = self.text
                 # Re-render the text.
                 self.txt_surface = FONT.render(self.text, True, self.color)
 
@@ -104,35 +122,38 @@ class InputBox:
 
 class infoInput:
     def __init__(self):
-        self.joueurs = ''
+        self.pseudo = ''
         self.room = ''
         self.mdp = ''    
 
 def main():
+    
     sio.connect('http://10.50.1.66:5000')
     clock = pg.time.Clock()
     state = infoInput()
-    input_box1 = InputBox(100, 50, 200, 32, 'joueurs', state)
-    input_box2 = InputBox(100, 150, 200, 32, 'room', state)
-    input_box3 = InputBox(100, 250, 200, 32, 'mdp', state)
-    input_boxes = [input_box1, input_box2, input_box3]
+    input_box1 = InputBox(100, 50, 200, 32, 'pseudo', state)
+    #input_box2 = InputBox(100, 150, 200, 32, 'room', state)
+    #input_box3 = InputBox(100, 250, 200, 32, 'mdp', state)
+    input_boxes = [input_box1]
+    #, input_box2, input_box3
     submit = SubmitButton(100, 325, 200, 50, state)
     boxes = input_boxes + [submit]
     done = False 
 
     screen.fill((255,255,255))
     screen.blit(text, textRect)
-    screen.blit(text3, textRect3)
-    screen.blit(text4, textRect4)
-    
+    screen.blit(text5, textRect5)
+    #screen.blit(text3, textRect3)
+    #screen.blit(text4, textRect4)
+
     for box in boxes:
         box.draw(screen)
     
-            
     while not done:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 done = True
+                sio.disconnect()
             for box in boxes:
                 box.handle_event(event)
                 box.update()
